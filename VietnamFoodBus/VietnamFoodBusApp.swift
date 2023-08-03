d43a0f82da75b444bfa1b36a8d1b18b3ceb33405
snowman72 <s3978681@rmit.edu.vn>
2023-08-03 09:36:18 +0700

    Initial Commit

---
+++ VietnamFoodBus/VietnamFoodBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VietnamFoodBusApp.swift
+//  VietnamFoodBus
+//
+//  Created by Vũ Minh Hà on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct VietnamFoodBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
